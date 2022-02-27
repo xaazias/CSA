2613820f004639c01d1f8659938bebca3aca7634
Александр Корольков <saizaax@MacBook-Air.local>
2022-02-28 00:37:27 +0300

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Logo from "./logo.png"
+
+function App() {
+  return (
+    <div className="App">
+      <div className="icon">
+        <img src={Logo} alt="icon" />
+      </div>
+      <h3 className="group">ИКБО-13-19</h3>
+      <p className="name">Корольков Александр</p>
+    </div>
+  )
+}
+
+export default App
